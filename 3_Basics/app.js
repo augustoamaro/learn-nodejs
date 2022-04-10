d7df861b5bf3ef9d16686fe700ad358aff497404
Augusto <augustoamaro@icloud.com>
2022-04-10 18:08:36 -0300

    Added express

--- 3_Basics/app.js
+++ 3_Basics/app.js
@@ -1,7 +1,21 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const port = 3000;
 
-const server = http.createServer(request);
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-server.listen(3000);
+const app = express();
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found!</h1>')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${3000}...`)
+})
