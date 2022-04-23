b13fdb40cd49fb0927cfbffdd5aa44f53ca94432
Augusto <augustoamaro@icloud.com>
2022-04-23 19:38:33 -0300

    Added express examples

--- 3_Basics/app.js
+++ 3_Basics/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const port = 3000;
@@ -8,12 +10,13 @@ const shopRoutes = require('./routes/shop')
 const app = express();
 
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found!</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(port, () => {
